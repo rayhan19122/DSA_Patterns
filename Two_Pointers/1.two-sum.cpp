4ef67b388a8dc1c32a2091433157e5b7fde87c85
Md Abu Rayhan <rayhan19122@gmail.com>
2026-01-13 12:13:21 +0600

    two sum Ac

--- Two_Pointers/1.two-sum.cpp
+++ Two_Pointers/1.two-sum.cpp
@@ -1,3 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
 /*
  * @lc app=leetcode id=1 lang=cpp
  *
@@ -8,8 +10,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]=i;
+        }
+
+        for(int i=0;i<nums.size();i++){
+            int req=target-nums[i];
+            if(mp.find(req)!=mp.end() && mp[req]!=i){
+                return {i,mp[req]};
+            }
+        }
+        return {};
+       
     }
 };
 // @lc code=end
-
