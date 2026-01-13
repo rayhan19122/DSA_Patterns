3e2f8c6747c9507fbf142f0d4b3e87bd408cfd8a
Md Abu Rayhan <rayhan19122@gmail.com>
2026-01-13 10:30:06 +0600

    added two ponter solution

--- Two_Pointers/167.two-sum-ii-input-array-is-sorted.cpp
+++ Two_Pointers/167.two-sum-ii-input-array-is-sorted.cpp
@@ -5,6 +5,8 @@
  */
 
 // @lc code=start
+
+// Using Binary Search
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
@@ -12,6 +14,21 @@ public:
             int val=target-numbers[i];
             int lo=lower_bound(numbers.begin()+i+1,numbers.end(),val)-numbers.begin();
             if(lo<numbers.size() && numbers[lo]==val) return {i+1, lo+1};
+        }
+        return {-1, -1};
+    }
+};
+
+// Using Two Pointers
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0,j=numbers.size()-1;
+        while(i<j){
+            long long sum=numbers[i]+numbers[j];
+            if(sum==target) return {i+1,j+1};
+            if(sum>target) j--;
+            else i++;
         }
         return {-1, -1};
     }
