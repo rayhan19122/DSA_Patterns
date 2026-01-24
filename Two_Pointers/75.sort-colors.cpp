ee6d92bc596fea67bc3fca16588bd8967bd67919
Md Abu Rayhan <rayhan19122@gmail.com>
2026-01-24 09:21:10 +0600

    Dutch National Flag Solved

---
+++ Two_Pointers/75.sort-colors.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode id=75 lang=cpp
+ *
+ * [75] Sort Colors
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        int mid=0;
+        
+        while (mid<=high)
+        {
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                mid++,low++;
+            }else if(nums[mid]==1) mid++;
+            else {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+        
+    }
+};
+// @lc code=end
+
